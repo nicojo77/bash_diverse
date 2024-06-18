61d82e34e29418b7726491b96de6c2459e092826
nicojo77 <ad4account@protonmail.com>
2024-06-18 16:53:21 +0200

    geturl.sh

---
+++ geturl.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#script:		geturl.sh
+#author:		anon
+#date:			18.06.2024
+#modification:	18.06.2024
+#purpose:
+#usage:			./geturl.sh
+
+#exit codes:
+#	0	successful exit
+
+main() {
+	echo "zeek-cut host uri <http.log | awk -F '\t' '{print\$1"/"\$2}' | sed 's#//#7#g'"
+	echo
+	zeek-cut host uri <http.log | awk -F '\t' '{print$1"/"$2}' | sed 's#//#7#g'
+}
+
+main
+
+exit 0
